ec03da66f87fb529efb69fd9f96b9e71b909e838
BlueSCar <radjewwj@gmail.com>
2018-09-19 21:55:25 -0400

    added a games REST endpoint

--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -1,5 +1,58 @@
 module.exports = (db) => {
     return {
+        getGames: async (req, res) => {
+            try {
+                if (!req.query.year) {
+                    req.status(400).send({
+                        error: 'A year parameter must be specified.'
+                    });
+
+                    return;
+                }
+
+                let filter = 'WHERE g.season = $1';
+                let params = [req.query.year];
+
+                let index = 2;
+
+                if (req.query.week) {
+                    filter += ` AND g.week = ${index}`;
+                    params.push(req.query.week);
+                    index++;
+                }
+
+                if (req.query.home) {
+                    filter += ` AND LOWER(home.school) = LOWER($${index})`;
+                    params.push(req.query.offense);
+                    index++;
+                }
+
+                if (req.query.away) {
+                    filter += ` AND LOWER(away.school) = LOWER($${index})`;
+                    params.push(req.query.defense);
+                    index++;
+                }
+
+                let games = await db.any(`
+                    SELECT g.id, g.season, g.week, g.season_type, g.start_date, g.neutral_site, g.conference_game, g.attendance, v.name as venue, home.school as home_team, gt.points as home_points, gt.line_scores as home_line_scores, away.school as away_team, gt2.points as away_points, gt2.line_scores as away_line_scores
+                    FROM game g
+                        INNER JOIN game_team gt ON g.id = gt.game_id AND gt.home_away = 'home'
+                        INNER JOIN team home ON gt.team_id = home.id
+                        INNER JOIN game_team gt2 ON g.id = gt2.game_id AND gt2.home_away = 'away'
+                        INNER JOIN team away ON gt2.team_id = away.id
+                        LEFT JOIN venue v ON g.venue_id = v.id
+                    ${filter}
+                    ORDER BY g.season, g.week, g.start_date
+            `, params);
+
+                res.send(games);
+            } catch (err) {
+                console.error(err);
+                res.status(500).send({
+                    error: 'Something went wrong.'
+                });
+            }
+        },
         getDrives: async (req, res) => {
                 try {
                     if (!req.query.year) {
--- app/game/game.route.js
+++ app/game/game.route.js
@@ -1,6 +1,7 @@
 module.exports = (app, db) => {
     const controller = require('./game.controller')(db);
 
+    app.route('/games').get(controller.getGames);
     app.route('/drives').get(controller.getDrives);
     app.route('/plays').get(controller.getPlays);
 }
