6b681077f20ce7bf03196fbf3aa658d2a53c3ee0
BlueSCar <radjewwj@gmail.com>
2019-08-21 22:06:19 +0000

    including team id in responses

--- app/team/team.controller.js
+++ app/team/team.controller.js
@@ -6,7 +6,7 @@ module.exports = (db) => {
                 let params = [req.query.conference];
 
                 let teams = await db.any(`
-                    SELECT t.school, t.mascot, t.abbreviation, c.name as conference, ct.division as division, ('#' || t.color) as color, ('#' || t.alt_color) as alt_color, t.images as logos
+                    SELECT t.id, t.school, t.mascot, t.abbreviation, c.name as conference, ct.division as division, ('#' || t.color) as color, ('#' || t.alt_color) as alt_color, t.images as logos
                     FROM team t
                         LEFT JOIN conference_team ct ON t.id = ct.team_id AND ct.end_year IS NULL
                         LEFT JOIN  conference c ON c.id = ct.conference_id
