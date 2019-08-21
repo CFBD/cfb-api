612999e00bfdfc65d37cd8c7594ba68005e1f74a
BlueSCar <radjewwj@gmail.com>
2019-08-21 18:05:31 -0400

    fbs teams

--- app/team/team.route.js
+++ app/team/team.route.js
@@ -2,6 +2,7 @@ module.exports = (app, db, cors) => {
     const controller = require('./team.controller')(db);
 
     app.route('/teams').get(cors, controller.getTeams);
+    app.route('/teams/fbs').get(cors, controller.getFBSTeams);
     app.route('/roster').get(cors, controller.getRoster);
     app.route('/conferences').get(cors, controller.getConferences);
     app.route('/talent').get(cors, controller.getTeamTalent);
