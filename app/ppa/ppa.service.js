b5d1dcf84692b397e6665e55dac56247b65b17f6
BlueSCar <radjewwj@gmail.com>
2019-08-26 00:01:11 -0400

    ppa fix

--- app/ppa/ppa.service.js
+++ app/ppa/ppa.service.js
@@ -4,9 +4,9 @@ module.exports = (db) => {
         let results = await db.any(`
                 SELECT (100 - yard_line) AS yardline, predicted_points
                 FROM ppa
-                WHERE down = 1 AND distance = 10
+                WHERE down = $1 AND distance = $2
                 ORDER BY yardline
-        `);
+        `, [down, distance]);
 
         return results.map(r => ({
             yardLine: r.yardline,
