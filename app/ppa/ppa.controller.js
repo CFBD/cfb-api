c4443ad4eb7435eeb429c7725bbdfdd5b90e6bc7
Bill Radjewski <bradjews@assurexhealth.com>
2019-10-10 14:27:09 -0400

    player ppa routes

--- app/ppa/ppa.controller.js
+++ app/ppa/ppa.controller.js
@@ -46,7 +46,7 @@ module.exports = (db) => {
             });
         }
     }
-    
+
     const getPPAByTeam = async (req, res) => {
         try {
             if (!req.query.year && !req.query.team) {
@@ -70,28 +70,101 @@ module.exports = (db) => {
     }
 
     const getPPAByGame = async (req, res) => {
-        if (!req.query.year) {
-            res.status(400).send({
-                error: 'year must be specified'
+        try {
+            if (!req.query.year) {
+                res.status(400).send({
+                    error: 'year must be specified'
+                });
+            } else if (req.query.year && !parseInt(req.query.year)) {
+                res.status(400).send({
+                    error: 'year must be numeric'
+                });
+            } else if (req.query.week && !parseInt(req.query.week)) {
+                res.status(400).send({
+                    error: 'week must be numeric'
+                });
+            } else {
+                const results = await service.getPPAByGame(req.query.year, req.query.team, req.query.conference, req.query.week);
+                res.send(results);
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({
+                error: 'Something went wrong.'
             });
-        } else if (req.query.year && !parseInt(req.query.year)) {
-            res.status(400).send({
-                error: 'year must be numeric'
-            });
-        } else if (req.query.week && !parseInt(req.query.week)) {
-            res.status(400).send({
-                error: 'week must be numeric'
-            });
-        } else {
-            const results = await service.getPPAByGame(req.query.year, req.query.team, req.query.conference, req.query.week);
-            res.send(results);
         }
-    }
+    };
+
+    const getPPAByPlayerGame = async (req, res) => {
+        try {
+            if (!req.query.week && !req.query.team) {
+                res.status(400).send({
+                    error: 'A week or team must be specified'
+                });
+            } else if (req.query.year && !parseInt(req.query.year)) {
+                res.status(400).send({
+                    error: 'year must be numeric'
+                });
+            } else if (req.query.week && !parseInt(req.query.week)) {
+                res.status(400).send({
+                    error: 'week must be numeric'
+                });
+            } else if (req.query.threshold && !parseInt(req.query.threshold)) {
+                res.status(400).send({
+                    error: 'threshold must by numeric'
+                });
+            } else if (req.query.playerId && !parseInt(req.query.playerId)) {
+                res.status(400).send({
+                    error: 'playerId must be numeric'
+                });
+            } else {
+                const results = await service.getPPAByPlayerGame(req.query.year, req.query.week, req.query.position, req.query.team, req.query.playerId, req.query.threshold);
+                res.send(results);
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({
+                error: 'Something went wrong.'
+            });
+        }
+    };
+
+    const getPPAByPlayerSeason = async (req, res) => {
+        try {
+            if (req.query.year && !parseInt(req.query.year)) {
+                res.status(400).send({
+                    error: 'year must be numeric'
+                });
+            } else if (req.query.week && !parseInt(req.query.week)) {
+                res.status(400).send({
+                    error: 'week must be numeric'
+                });
+            } else if (req.query.threshold && !parseInt(req.query.threshold)) {
+                res.status(400).send({
+                    error: 'threshold must by numeric'
+                });
+            } else if (req.query.playerId && !parseInt(req.query.playerId)) {
+                res.status(400).send({
+                    error: 'playerId must be numeric'
+                });
+            } else {
+                const results = await service.getPPAByPlayerSeason(req.query.year, req.query.conference, req.query.position, req.query.team, req.query.playerId, req.query.threshold);
+                res.send(results);
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send({
+                error: 'Something went wrong.'
+            });
+        }
+    };
 
     return {
         getPP,
         getPPAByTeam,
         getWP,
-        getPPAByGame
+        getPPAByGame,
+        getPPAByPlayerGame,
+        getPPAByPlayerSeason
     }
 }
