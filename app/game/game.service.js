a0426a499ec6d9c31db62cf6a1020024be65a9c4
Bill Radjewski <radjewwj@gmail.com>
2020-06-18 20:11:05 -0400

    fixing swagger doc issue

--- app/game/game.service.js
+++ app/game/game.service.js
@@ -10,7 +10,7 @@ module.exports = (db) => {
             index++;
         }
 
-        if (seasonType) {
+        if (seasonType && seasonType.toLowerCase() !== 'both') {
             filters.push(`g.season_type = ${seasonType}`);
             params.push(seasonType);
             index++;
