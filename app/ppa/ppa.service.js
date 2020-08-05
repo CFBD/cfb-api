7bcc65f04188b78fc296b1d520aac218e61306b1
Bill Radjewski <radjewwj@gmail.com>
2020-08-04 22:08:57 -0400

    revamped WP

--- app/ppa/ppa.service.js
+++ app/ppa/ppa.service.js
@@ -1,11 +1,8 @@
-const synaptic = require('synaptic');
 const gaussian = require('gaussian');
 const gaussianDistro = gaussian(0, Math.pow(17, 2));
+const axios = require('axios');
 
 module.exports = (db) => {
-    const Architect = synaptic.Architect;
-    const wpNetwork = Architect.Perceptron.fromJSON(require('./wpNetwork'));
-    const wpKeys = require('./wpKeys');
 
     const getPP = async (down, distance) => {
         let results = await db.any(`
@@ -21,111 +18,63 @@ module.exports = (db) => {
         }));
     };
 
-    const renormalizeData = (inputs) => {
-        let inputMins = wpKeys.input.mins;
-        let inputMaxes = wpKeys.input.maxes;
-        let inputNorm = [];
-        for (let i = 0; i < inputs.length; i++) {
-            inputNorm.push((inputs[i] - inputMins[i]) / (inputMaxes[i] - inputMins[i]));
-        }
-
-        // datum.normalizedInput = inputNorm;
-
-        return inputNorm;
-    }
-
-    const getWP = async (gameId, includeSpread = true) => {
+    const getWP = async (gameId) => {
         let plays = await db.any(`
-        SELECT 	g.id,
-		        p.id AS play_id,
-                p.play_text,
-                home_team.id AS home_id,
-                home_team.school AS home,
-                away_team.id AS away_id,
-                away_team.school AS away,
-                COALESCE(gl.spread, 0) AS spread,
-                CASE 
-                    WHEN home.team_id = p.offense_id AND p.scoring = false THEN true 
-                    WHEN home.team_id = p.defense_id AND p.scoring = true THEN true
-                    ELSE false 
-                END AS has_ball,
-                p.home_score,
-                p.away_score,
-                (((4 - p.period) * 60 * 15) + EXTRACT(epoch FROM p.clock)) as time_remaining,
-                CASE
-                    WHEN (home.team_id = p.offense_id) OR (away.team_id = p.defense_id) THEN (100 - p.yard_line)
-                    ELSE p.yard_line 
-                END AS yard_line,
-                p.down,
-                p.distance,
-                home.winner,
-                ROW_NUMBER() OVER(ORDER BY p.period, p.clock DESC, p.id) AS play_number
-            FROM game AS g
-            INNER JOIN game_team AS home ON g.id = home.game_id AND home.home_away = 'home'
-            INNER JOIN team AS home_team ON home.team_id = home_team.id
-            INNER JOIN game_team AS away ON g.id = away.game_id AND away.home_away = 'away'
-            INNER JOIN team AS away_team ON away.team_id = away_team.id
-            INNER JOIN drive AS d ON g.id = d.game_id
-            INNER JOIN play AS p 
-                ON d.id = p.drive_id 
-                    AND p.play_type_id NOT IN (12,13,15,16,21,43,53,56,57,61,62,65,66,999,78)
-                    AND p.period < 5
-                    AND p.yard_line <= 99 
-                    AND p.yard_line >= 1
-                    AND p.down > 0 
-                    AND p.down < 5 
-                    AND p.distance <= CASE WHEN (home.team_id = p.offense_id) OR (away.team_id = p.defense_id) THEN (100 - p.yard_line) ELSE p.yard_line END
-                    AND p.distance >= 1
-            LEFT JOIN game_lines AS gl ON g.id = gl.game_id AND gl.lines_provider_id = 1004
-        WHERE g.id = $1
-        ORDER BY p.period, p.clock DESC
+            SELECT 	g.id,
+                    home_team.id AS home_id,
+                    home_team.school AS home,
+                    away_team.id AS away_id,
+                    away_team.school AS away,
+                    COALESCE(gl.spread, 0) AS spread,
+                    CASE 
+                        WHEN home.team_id = p.offense_id AND p.scoring = false THEN true
+                        WHEN home.team_id = p.defense_id AND p.scoring = true THEN false
+                        ELSE false
+                    END AS home_ball,
+                    p.id AS play_id,
+                    ROW_NUMBER() OVER(ORDER BY d.drive_number, p.play_number) - 1 AS play_number,
+                    p.play_text,
+                    p.home_score,
+                    p.away_score,
+                    p.period,
+                    p.clock,
+                    p.down,
+                    p.distance,
+                    CASE
+                        WHEN (home.team_id = p.offense_id) OR (away.team_id = p.defense_id) THEN (100 - p.yard_line)
+                        ELSE p.yard_line 
+                    END AS yards_to_goal,
+                    p.home_win_prob,
+                    home.winner AS home_winner
+                FROM game AS g
+                INNER JOIN game_team AS home ON g.id = home.game_id AND home.home_away = 'home'
+                INNER JOIN team AS home_team ON home.team_id = home_team.id
+                INNER JOIN game_team AS away ON g.id = away.game_id AND away.home_away = 'away'
+                INNER JOIN team AS away_team ON away.team_id = away_team.id
+                INNER JOIN drive AS d ON g.id = d.game_id
+                INNER JOIN play AS p 
+                    ON d.id = p.drive_id 
+                        AND p.play_type_id NOT IN (12,13,15,16,21,43,53,56,57,61,62,65,66,999,78)
+                        AND p.yard_line <= 99 
+                        AND p.yard_line >= 1
+                        AND p.down > 0 
+                        AND p.down < 5 
+                        AND p.distance <= CASE WHEN (home.team_id = p.offense_id) OR (away.team_id = p.defense_id) THEN (100 - p.yard_line) ELSE p.yard_line END
+                        AND p.distance >= 1
+                        AND p.home_win_prob IS NOT NULL
+                LEFT JOIN game_lines AS gl ON g.id = gl.game_id AND gl.lines_provider_id = 1004
+            WHERE g.id = $1
+            ORDER BY d.drive_number, p.play_number DESC
     `, [gameId]);
 
         if (plays && plays.length) {
-            let first = plays[0];
-            plays = [{
-                    gameId: first.gameId,
-                    play_id: 0,
-                    play_text: 'Game start',
-                    home_id: first.home_id,
-                    home: first.home,
-                    away_id: first.away_id,
-                    away: first.away,
-                    spread: first.spread,
-                    has_ball: false,
-                    home_score: 0,
-                    away_score: 0,
-                    time_remaining: 3600,
-                    yard_line: 65,
-                    down: 1,
-                    distance: 10,
-                    play_number: 0
-                },
-                ...plays
-            ];
-
-            for (let play of plays) {
-                play.spread *= (play.time_remaining / 3600) * (includeSpread == true || includeSpread == 'true' ? 1 : 0);
-            }
-
-            for (let i = 0; i < plays.length; i++) {
-                let play = plays[i];
-                let playInputs = [
-                    parseInt(play.spread), // spread
-                    play.has_ball ? 1 : 0, // possession
-                    parseInt(play.home_score), // home points
-                    parseInt(play.away_score), // opp_points
-                    parseInt(play.time_remaining), // seconds left
-                    parseInt(play.yard_line), // yards to goal
-                    parseInt(play.down), // down
-                    parseInt(play.distance) // distance
-                ];
-                play.homeWinProb = wpNetwork.activate(renormalizeData(playInputs))[0];
-            }
+            plays = plays.sort((a,b) => {
+                return parseInt(a.play_number) < parseInt(b.play_number) ? -1 : 1;
+            });
 
             let last = plays[plays.length - 1];
-            if (last.time_remaining == 0) {
-                last.homeWinProb = last.winner ? 1 : 0;
+            if (last.game_seconds_remaining == 0) {
+                last.win_prob = last.home_winner ? 1 : 0;
             } else {
                 plays = [
                     ...plays,
@@ -138,14 +87,13 @@ module.exports = (db) => {
                         away_id: last.away_id,
                         away: last.away,
                         spread: last.spread,
-                        has_ball: last.has_ball,
+                        home_ball: last.home_ball,
                         home_score: last.home_score,
                         away_score: last.away_score,
-                        time_remaining: 0,
                         yard_line: 65,
                         down: 0,
                         distance: 0,
-                        homeWinProb: last.winner ? 1 : 0,
+                        home_win_prob: last.home_winner ? 1 : 0,
                         play_number: (parseInt(last.play_number) + 1)
                     }
                 ]
@@ -161,16 +109,18 @@ module.exports = (db) => {
             awayId: p.away_id,
             away: p.away,
             spread: p.spread,
-            homeBall: p.has_ball,
+            homeBall: p.home_ball,
             homeScore: p.home_score,
             awayScore: p.away_score,
-            timeRemaining: p.time_remaining,
+            timeRemaining: p.game_seconds_remaining,
             yardLine: p.yard_line,
             down: p.down,
             distance: p.distance,
-            homeWinProb: p.homeWinProb,
-            playNumber: p.play_number
-        }));
+            homeWinProb: p.home_win_prob,
+            playNumber: parseInt(p.play_number)
+        })).sort((a,b) => {
+            return a.playNumber < b.playNumber ? -1 : 1;
+        });
     }
 
     const getPPAByTeam = async (year, team, conference, excludeGarbageTime) => {
