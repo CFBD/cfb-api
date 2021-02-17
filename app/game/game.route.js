5da64abcce06c8db3a0757adf2a8b539defbb0f3
Bill Radjewski <radjewwj@gmail.com>
2021-02-17 09:12:00 -0500

    temp disabling a couple of endpoints to mitigate attacks

--- app/game/game.route.js
+++ app/game/game.route.js
@@ -2,9 +2,9 @@ module.exports = (app, db, cors, speedLimiter, Sentry) => {
     const controller = require('./game.controller')(db, Sentry);
 
     app.route('/games').get(cors, controller.getGames);
-    app.route('/games/teams').get(cors, controller.getTeamStats);
+    // app.route('/games/teams').get(cors, controller.getTeamStats);
     app.route('/drives').get(cors, speedLimiter, controller.getDrives);
-    app.route('/games/players').get(cors, controller.getPlayerStats);
+    // app.route('/games/players').get(cors, controller.getPlayerStats);
     app.route('/records').get(cors, controller.getRecords);
     app.route('/games/media').get(cors, controller.getMedia);
     app.route('/calendar').get(cors, controller.getCalendar);
