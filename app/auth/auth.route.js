f530ea9a5009d9c6e1bd4af1e3d7d6672c5722ed
Bill Radjewski <radjewwj@gmail.com>
2021-02-20 16:55:50 -0500

    prevent bruteforcing registration

--- app/auth/auth.route.js
+++ app/auth/auth.route.js
@@ -1,7 +1,7 @@
-module.exports = (app, authDb, cors, Sentry) => {
+module.exports = (app, authDb, cors, Sentry, brute) => {
     const controller = require('./auth.controller')(Sentry);
 
-    app.route('/auth/key').post(cors, controller.generateKey);
+    app.route('/auth/key').post(cors, brute.prevent, controller.generateKey);
     app.route('/test').get(cors, (req, res) => {
         res.send({
             origin: req.get('origin'),
