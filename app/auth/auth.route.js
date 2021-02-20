f41944eecb3bf5a14e5a344b64a3ceb8a77a3572
Bill Radjewski <radjewwj@gmail.com>
2021-02-20 13:36:36 -0500

    forgot cors on test endpoint

--- app/auth/auth.route.js
+++ app/auth/auth.route.js
@@ -2,7 +2,7 @@ module.exports = (app, authDb, cors, Sentry) => {
     const controller = require('./auth.controller')(Sentry);
 
     app.route('/auth/key').post(cors, controller.generateKey);
-    app.route('/test').get((req, res) => {
+    app.route('/test').get(cors, (req, res) => {
         res.send({
             origin: req.get('origin'),
             host: req.get('host')
