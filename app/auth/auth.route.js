4d6e8db18d54ce14dbfd302d04b9585b782e942c
Bill Radjewski <radjewwj@gmail.com>
2021-02-20 13:23:36 -0500

    test route

--- app/auth/auth.route.js
+++ app/auth/auth.route.js
@@ -2,4 +2,10 @@ module.exports = (app, authDb, cors, Sentry) => {
     const controller = require('./auth.controller')(Sentry);
 
     app.route('/auth/key').post(cors, controller.generateKey);
+    app.route('/test').get((req, res) => {
+        res.send({
+            origin: req.get('origin'),
+            host: req.get('host')
+        });
+    });
 };
