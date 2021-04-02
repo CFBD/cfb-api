ffc83337d934b7a10396c8f4f8d5381e2168c921
Bill Radjewski <radjewwj@gmail.com>
2021-04-02 19:36:50 -0400

    adding more metrics

--- config/apm.js
+++ config/apm.js
@@ -3,9 +3,9 @@ module.exports = (db) => {
         if (req.user && req.user.id) {
             try {
                 await db.none(`
-                INSERT INTO metrics (user_id, endpoint, query)
+                INSERT INTO metrics (user_id, endpoint, query, user_agent)
                 VALUES ($1, $2, $3)
-            `, [req.user.id, req.sws.api_path, req.query]);
+            `, [req.user.id, req.sws.api_path, req.query, req.get('user-agent')]);
             } catch (err) {
                 console.error(err);
             }
